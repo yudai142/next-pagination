f848347e72c452ea72263828719590c269e5f630
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-10-07 13:11:36 +0900

    APIからリスト表示

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,39 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 const Home = () => {
+  
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(()=>{
+    const getPosts = async () =>{
+      const {data:res} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res);
+    }
+    getPosts();
+  },[])
   return ( 
     <div className="container">
-      <h2>Hello World</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {posts.map(post =>
+            <tr key={post.id}>
+              <td>{post.id}</td>
+              <td>{post.title}</td>
+              <td>
+                <button className="btn btn-danger btn-sm">Delete</button>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
